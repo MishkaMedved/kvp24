1f5fe007c9f7cafe7e29898b6b685b1a5531a82a
mixail <mixail.228@bk.ru>
2024-11-11 22:25:26 +0400

    Создал проект

---
+++ src/main/java/ru/mixail/kvp24/Kvp24Application.java
@@ -0,0 +1,13 @@
+package ru.mixail.kvp24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kvp24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kvp24Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mixail/kvp24/Kvp24ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mixail.kvp24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kvp24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
